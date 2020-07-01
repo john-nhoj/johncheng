f60b29957bfcb7b9b7e0d7d33a27bde74c4ebd72
John Cheng <john.cheng@futurice.com>
2020-07-01 17:07:19 +0300

    Update title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `John Cheng | Welcome`,
+    description: `This is my personal website`,
+    author: `@john-nhoj`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
