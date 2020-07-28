6b0460d6d632c91a85de9cc7dc2137e9685054cd
John Cheng <john.cheng@futurice.com>
2020-07-28 20:00:03 +0300

    Use module alias

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,12 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
+const path = require('path');
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@pages': path.resolve(__dirname, 'src/pages'),
+        '@styles': path.resolve(__dirname, 'src/styles'),
+      },
+    },
+  });
+};
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -9,9 +9,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useStaticQuery, graphql } from 'gatsby';
 
-import Header from './header';
-import './layout.css';
-
+import Header from '@components/header';
+import '@styles/layout.css';
 type Props = {
   children: React.ReactNode;
 };
