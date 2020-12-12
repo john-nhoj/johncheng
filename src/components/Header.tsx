9211ec09ce0a807cb91a1557eb15e444d5d817ab
John Cheng <25937809+john-nhoj@users.noreply.github.com>
2020-12-12 18:43:14 +0200

    Feature/header (#10)
    * Add components path alias
    
    * Make about and index simple
    
    * Add header to global app

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import { link } from "fs";
+import React from "react";
+
+type HeaderLinkProps = {
+  label: string;
+  link: string;
+};
+
+const HeaderLink = ({ label, link }: HeaderLinkProps) => (
+  <a href={link} className="p-4 h-full">
+    {label}
+  </a>
+);
+
+export const Header = () => (
+  <header className="bg-black text-white flex flex-wrap">
+    <HeaderLink label="Home" link="/" />
+    <HeaderLink label="About" link="/about" />
+    <HeaderLink label="Blog" link="/blog" />
+  </header>
+);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,14 @@
 import "styles/global.css";
+import type { AppProps } from "next/app";
+import { Header } from "components/Header";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <div>
+      <Header />
+      <Component {...pageProps} />
+    </div>
+  );
 }
-
-// Only uncomment this method if you have blocking data requirements for
-// every single page in your application. This disables the ability to
-// perform automatic static optimization, causing every page in your app to
-// be server-side rendered.
-//
-// MyApp.getInitialProps = async (appContext) => {
-//   // calls page's `getInitialProps` and fills `appProps.pageProps`
-//   const appProps = await App.getInitialProps(appContext);
-//
-//   return { ...appProps }
-// }
 
 export default MyApp;
