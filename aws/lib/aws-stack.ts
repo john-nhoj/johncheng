e9ba739a3041f1b2ae0cb473c42ea35f1a2134dd
John Cheng <john.cheng@futurice.com>
2020-12-24 00:15:10 +0200

    Add docker command to build

--- aws/lib/aws-stack.ts
+++ aws/lib/aws-stack.ts
@@ -10,6 +10,7 @@ import * as codepipeline from '@aws-cdk/aws-codepipeline';
 import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
 import * as codeBuild from '@aws-cdk/aws-codebuild';
 import * as ssm from '@aws-cdk/aws-ssm';
+import * as ecr from '@aws-cdk/aws-ecr';
 
 export interface PipelineStackProps extends cdk.StackProps {
   readonly githubToken: string;
@@ -71,8 +72,48 @@ export class AwsStack extends cdk.Stack {
 
     const sourceOutput = new codepipeline.Artifact('SrcOutput');
     const cdkBuildOutput = new codepipeline.Artifact('CdkBuildOutput');
+    const ecrRepo = new ecr.Repository(this, 'ECRRepo');
+    ecrRepo.grantPull(loadBalancedFargateService.taskDefinition.executionRole!);
+    const containerName = loadBalancedFargateService.taskDefinition
+      .defaultContainer!.containerName;
 
-    const project = new codeBuild.PipelineProject(this, 'Project', {});
+    const project = new codeBuild.PipelineProject(this, 'Project', {
+      buildSpec: codeBuild.BuildSpec.fromObject({
+        version: '0.2',
+        phases: {
+          install: {
+            'runtime-versions': {
+              nodejs: 14,
+            },
+            commands: ['npm install -g npm', 'n stable', 'npm install'],
+          },
+          pre_build: {
+            commands: [
+              'aws --version',
+              "$(aws ecr get-login --region ${AWS_DEFAULT_REGION} --no-include-email |  sed 's|https://||')",
+              'COMMIT_HASH=$(echo $CODEBUILD_RESOLVED_SOURCE_VERSION | cut -c 1-7)',
+              'IMAGE_TAG=${COMMIT_HASH:=latest}',
+              'npm run test',
+            ],
+          },
+          build: {
+            commands: [
+              'npm run build',
+              'docker push $REPOSITORY_URI:latest',
+              'docker push $REPOSITORY_URI:$IMAGE_TAG',
+              'printf "[{\\"name\\":\\"${CONTAINER_NAME}\\",\\"imageUri\\":\\"${REPOSITORY_URI}:latest\\"}]" > imagedefinitions.json',
+            ],
+          },
+        },
+        artifacts: {
+          files: ['imagedefinitions.json'],
+        },
+      }),
+      environmentVariables: {
+        REPOSITORY_URI: { value: ecrRepo.repositoryArn },
+        CONTAINER_NAME: { value: containerName },
+      },
+    });
 
     const secret = cdk.SecretValue.secretsManager(
       '/johncheng/dev/GITHUB_TOKEN'
