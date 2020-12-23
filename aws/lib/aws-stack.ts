3ffa0fb6c8642297125fd70c93130ea6885c44c2
John Cheng <john.cheng@futurice.com>
2020-12-23 20:41:43 +0200

    Create codepipeline

--- aws/lib/aws-stack.ts
+++ aws/lib/aws-stack.ts
@@ -6,9 +6,17 @@ import * as cfOrigins from '@aws-cdk/aws-cloudfront-origins';
 import * as ec2 from '@aws-cdk/aws-ec2';
 import * as ecs from '@aws-cdk/aws-ecs';
 import * as ecsPatterns from '@aws-cdk/aws-ecs-patterns';
+import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
+import * as codeBuild from '@aws-cdk/aws-codebuild';
+import * as ssm from '@aws-cdk/aws-ssm';
+
+export interface PipelineStackProps extends cdk.StackProps {
+  readonly githubToken: string;
+}
 
 export class AwsStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props?: PipelineStackProps) {
     super(scope, id, props);
 
     // Route53
@@ -59,6 +67,55 @@ export class AwsStack extends cdk.Stack {
           loadBalancedFargateService.loadBalancer
         ),
       },
+    });
+
+    const sourceOutput = new codepipeline.Artifact('SrcOutput');
+    const cdkBuildOutput = new codepipeline.Artifact('CdkBuildOutput');
+
+    const project = new codeBuild.PipelineProject(this, 'Project', {});
+
+    const secret = cdk.SecretValue.secretsManager(
+      '/johncheng/dev/GITHUB_TOKEN'
+    );
+    const repo = ssm.StringParameter.valueForStringParameter(
+      this,
+      '/johncheng/dev/GITHUB_REPO'
+    );
+    const owner = ssm.StringParameter.valueForStringParameter(
+      this,
+      '/johncheng/dev/GITHUB_OWNER'
+    );
+
+    const pipeline = new codepipeline.Pipeline(this, 'MyFirstPipeline', {
+      pipelineName: 'MyPipeline',
+      crossAccountKeys: false,
+      stages: [
+        {
+          stageName: 'Source',
+          actions: [
+            new codepipeline_actions.GitHubSourceAction({
+              actionName: 'Checkout',
+              output: sourceOutput,
+              owner,
+              repo,
+              oauthToken: secret,
+              trigger: codepipeline_actions.GitHubTrigger.WEBHOOK,
+              branch: 'deploy-prod',
+            }),
+          ],
+        },
+        {
+          stageName: 'Build',
+          actions: [
+            new codepipeline_actions.CodeBuildAction({
+              actionName: 'Html_Build',
+              project,
+              input: sourceOutput,
+              outputs: [cdkBuildOutput],
+            }),
+          ],
+        },
+      ],
     });
   }
 }
