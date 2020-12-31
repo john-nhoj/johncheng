e9237c6241b635603e79229a96bc925c441b4a49
John Cheng <cheng.john@pm.me>
2020-12-31 11:18:50 +0200

    Add gutter for main component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,15 @@ import 'styles/global.css';
 import 'styles/typography.css';
 import type { AppProps } from 'next/app';
 import { Header } from 'components/Header/Header';
+import classNames from 'classnames';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
       <Header />
-      <Component {...pageProps} />
+      <div className={classNames('sm:px-20', 'md:px-40', 'xl:px-gutter')}>
+        <Component {...pageProps} />
+      </div>
     </div>
   );
 }
