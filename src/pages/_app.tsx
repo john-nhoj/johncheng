cc6b307a2b704f8d1cada5de5002e0d6ec6df09e
John Cheng <cheng.john@pm.me>
2020-12-31 11:39:53 +0200

    Add wrapper classes for paddings and gutter

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import 'styles/global.css';
 import 'styles/typography.css';
+import 'styles/layout.css';
 import type { AppProps } from 'next/app';
 import { Header } from 'components/Header/Header';
 import classNames from 'classnames';
@@ -8,7 +9,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
       <Header />
-      <main className={classNames('sm:px-20', 'md:px-40', 'xl:px-gutter')}>
+      <main className={classNames('wrapper-gutter')}>
         <Component {...pageProps} />
       </main>
     </div>
