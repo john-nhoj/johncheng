17dd66e1484773f448eddea4effa7978a91a3d96
John Cheng <cheng.john@pm.me>
2020-12-31 17:46:36 +0200

    Add image and button to toggle between leisure and business me

--- src/components/Introduction/Introduction.tsx
+++ src/components/Introduction/Introduction.tsx
@@ -1,25 +1,64 @@
 import classNames from 'classnames';
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
+import { PictureToggleButton } from 'components/Buttons/PictureToggleButton';
 
 const Introduction = () => {
+  const [showChillPicture, togglePicture] = useState(false);
+
+  const showBusinessPicture = () => togglePicture(false);
+  const showPicture = () => togglePicture(true);
+
   return (
-    <div className={classNames('flex', 'layout__padding--full', 'mt-8')}>
+    <div
+      className={classNames(
+        'flex',
+        'layout__padding--full',
+        'mt-8',
+        'justify-center',
+        'items=center'
+      )}
+    >
       <div
-        className={classNames('flex', 'flex-col', 'flex-1', 'justify-center')}
+        className={classNames(
+          'flex',
+          'flex-col',
+          'justify-center',
+          'items-center',
+          'text-center'
+        )}
       >
         <h2 className={classNames('text-5xl', 'font-bold')}>John Cheng</h2>
-        <p className={classNames('uppercase', 'mt-2')}>
-          Software Developer and badminton player
+        <p
+          className={classNames(
+            'uppercase',
+            'mt-4',
+            'flex',
+            'flex-col',
+            'xl:flex-row',
+            'items-center'
+          )}
+        >
+          <PictureToggleButton
+            onClickHandler={showBusinessPicture}
+            label="Software Developer"
+          />{' '}
+          and{' '}
+          <PictureToggleButton
+            onClickHandler={showPicture}
+            label="badminton player"
+          />
         </p>
       </div>
-      <Image
-        src="/johncheng.jpg"
-        alt="Picture of site owner"
-        width={300}
-        height={300}
-        className="rounded-xl"
-      />
+      <div className={classNames('hidden', 'md:flex', 'ml-24')}>
+        <Image
+          src={showChillPicture ? '/chill_me.jpeg' : '/johncheng.jpg'}
+          alt="Picture of site owner"
+          width={300}
+          height={300}
+          className={classNames('rounded-xl')}
+        />
+      </div>
     </div>
   );
 };
