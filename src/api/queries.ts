da0f7862f5b13a5a7e4d9f5635b992d4f8140ea9
John Cheng <25937809+john-nhoj@users.noreply.github.com>
2021-02-27 12:26:13 +0200

    20210227 (#50)
    * Page/home (#42)
    
    * Remove whitespace
    
    * Add personal picture
    
    * Add introduction card
    
    * Add picture non business
    
    * Update margins to be more user friendly
    
    * Add custom zIndex and additional background color
    
    * Add image and button to toggle between leisure and business me
    
    * Add snapshots for picture toggle button
    
    * Add tests and snapshots for introduction card
    
    * Logo/add new logo (#44)
    
    * Upload new brand logo
    
    * Update with new logo
    
    * Asset/favicon (#46)
    
    * Update favicon to use logo
    
    * Update package-lock
    
    * Feature/contentful (#47)
    
    * Add Contentful dependencies
    
    * Add path alias for api
    
    * Add general client to connect with Contentful
    
    * Add query to get a single post from slug
    
    * Add wip page for blog post
    
    * Add schema for blogpost
    
    * Dependencies/monthly update (#48)
    
    * Make versions to be exact and update all dependencies
    
    * Remove prettier plugin
    
    See issue: https://github.com/prettier/eslint-config-prettier/blob/main/CHANGELOG.md#version-800-2021-02-21
    
    * Update config for husky v5
    
    * Update with NextJS image updates
    
    * Update with installed husky (#49)

---
+++ src/api/queries.ts
@@ -0,0 +1,28 @@
+import { postFields } from './schemas';
+
+const { CONTENTFUL_ACCESS_TOKEN, CONTENTFUL_SPACE_ID } = process.env;
+
+export const getPost = (slug: string) => {
+  const query = `
+  {
+    blogPostCollection(where: {slug: "${slug}"}, limit: 1) {
+      items {
+        ${postFields}
+      }
+    }
+  }
+  `;
+  return fetch(
+    `https://graphql.contentful.com/content/v1/spaces/${CONTENTFUL_SPACE_ID}`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${CONTENTFUL_ACCESS_TOKEN}`,
+      },
+      body: JSON.stringify({ query }),
+    }
+  ).then((res) => {
+    return res.json();
+  });
+};
