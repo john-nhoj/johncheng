3631d56ecdce918e2dcc5370d137a16088a1841a
John Cheng <cheng.john@pm.me>
2021-03-16 15:47:32 +0200

    Add Terraform configs

---
+++ gcloud/variables.tf
@@ -0,0 +1,19 @@
+variable "project_id" {
+  description = "Project id"
+}
+
+variable "region" {
+  description = "Project region"
+}
+
+variable "zone" {
+  description = "Project zone"
+}
+
+variable "repository_name" {
+  description = "Name of the repository for the website"
+}
+
+variable "cluster_name" {
+  description = "Name of the Kubernetes cluster"
+}
