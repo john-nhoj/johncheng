cfbbee1db35a28d17207466e187b4d27114ee00b
John Cheng <cheng.john@pm.me>
2021-03-16 23:11:12 +0200

    Reenable posts

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,30 +1,29 @@
 import React from 'react';
-// import { client } from 'api/client';
-// import { getPost } from 'api/queries';
+import { client } from 'api/client';
+import { getPost } from 'api/queries';
 
-// const Post = ({ post }) => (
-//   <div>
-//     <h1>{post.title}</h1>
-//     <p>{post.body}</p>
-//   </div>
-// );
-const Post = () => <div>This is a post page</div>;
+const Post = ({ post }) => (
+  <div>
+    <h1>{post.title}</h1>
+    <p>{post.body}</p>
+  </div>
+);
 
-// export const getStaticProps = async ({ params }) => {
-//   console.debug('Fetching static props');
-//   const post = await getPost(params.slug);
-//   return { props: { post: post?.data?.blogPostCollection?.items?.[0] } };
-// };
+export const getStaticProps = async ({ params }) => {
+  console.debug('Fetching static props');
+  const post = await getPost(params.slug);
+  return { props: { post: post?.data?.blogPostCollection?.items?.[0] } };
+};
 
-// interface PostEntry {
-//   title: string;
-//   slug?: string;
-// }
-// export const getStaticPaths = async () => {
-//   const allEntries = await client.getEntries<PostEntry>();
-//   const posts = allEntries.items ?? [];
-//   const paths = posts.map((post) => `/posts/${post.fields.slug}`);
-//   return { paths, fallback: false };
-// };
+interface PostEntry {
+  title: string;
+  slug?: string;
+}
+export const getStaticPaths = async () => {
+  const allEntries = await client.getEntries<PostEntry>();
+  const posts = allEntries.items ?? [];
+  const paths = posts.map((post) => `/posts/${post.fields.slug}`);
+  return { paths, fallback: false };
+};
 
 export default Post;
