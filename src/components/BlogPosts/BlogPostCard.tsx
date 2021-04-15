348f58851fe17a75ec3f2a06a3dc1f41b3cc55c0
John Cheng <cheng.john@pm.me>
2021-04-15 23:32:35 +0300

    Style card

--- src/components/BlogPosts/BlogPostCard.tsx
+++ src/components/BlogPosts/BlogPostCard.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import Image from 'next/image';
 import { Post } from 'types/contentful';
 import classNames from 'classnames';
+import { Link } from 'components/Link/Link';
 
 interface BlogPostCardProps {
   post: Post;
 }
 
 const BlogPostCard = ({ post }: BlogPostCardProps) => (
-  <section className={classNames('w-80', 'm-4', 'rounded-lg', 'shadow-2xl')}>
+  <Link
+    href={`/posts/${post.slug}`}
+    className={classNames('w-80', 'm-4', 'rounded-lg', 'shadow-2xl')}
+  >
     <div className={classNames('relative', 'h-60')}>
       <Image
         src={post.heroImage.url}
@@ -22,14 +26,15 @@ const BlogPostCard = ({ post }: BlogPostCardProps) => (
         'border',
         'border-t-0',
         'rounded-lg',
-        'rounded-t-none'
+        'rounded-t-none',
+        'p-4'
       )}
     >
-      <h3>{post.title}</h3>
-      <p>{post.description}</p>
-      <p>{post.publishDate}</p>
+      <h3 className={classNames('underline', 'text-lg')}>{post.title}</h3>
+      <p className={classNames('mt-4')}>{post.description}</p>
+      <p className={classNames('italic', 'mt-4')}>{post.publishDate}</p>
     </div>
-  </section>
+  </Link>
 );
 
 export { BlogPostCard };
