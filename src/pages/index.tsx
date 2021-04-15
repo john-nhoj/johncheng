52a3d7fd53e25165511bd8643c3f17163a61d22c
John Cheng <cheng.john@pm.me>
2021-04-15 20:56:26 +0300

    Add blogpost section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
-import { Introduction } from 'components/Introduction/Introduction';
 import Head from 'next/head';
+import { Introduction } from 'components/Introduction/Introduction';
+import { BlogPosts } from 'components/BlogPosts';
+import { getBlogPostsList } from 'api/queries';
+import { Post } from 'src/types/contentful';
 
-const Home = () => (
+interface Props {
+  posts: Array<Post>;
+}
+
+const Home = ({ posts }: Props) => (
   <>
     <Head>
       <title>John Cheng – Personal website</title>
       <meta name="description" content="Welcome page" />
     </Head>
     <Introduction />
+    <BlogPosts posts={posts} />
   </>
 );
+
+export const getStaticProps = async () => {
+  console.debug('Fetching blogposts');
+  const allEntries = await getBlogPostsList();
+  const posts = allEntries?.data?.blogPostCollection?.items ?? [];
+  return { props: { posts } };
+};
 
 export default Home;
