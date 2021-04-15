30a432f6966b270648b8661f2eb82a76064794d9
John Cheng <cheng.john@pm.me>
2021-04-15 18:16:44 +0300

    Remove unneeded classnames

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,12 @@ import 'styles/typography.css';
 import 'styles/layout.css';
 import type { AppProps } from 'next/app';
 import { Header } from 'components/Header/Header';
-import classNames from 'classnames';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Header />
-      <main className={classNames('flex', 'flex-grow', 'flex-col')}>
+      <main>
         <Component {...pageProps} />
       </main>
     </>
